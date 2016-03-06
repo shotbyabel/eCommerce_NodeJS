9fb8804c065b6dc892a73349fa24ef4991e700be
kimo5532 <shotbyabel@gmail.com>
2016-03-05 18:41:01 -0800

    Finish product view route and logic

--- routes/main-route.js
+++ routes/main-route.js
@@ -1,5 +1,6 @@
 var router    = require('express').Router();
-// var User      = require('../models/user');
+var User      = require('../models/user');
+var Product   = require('../models/product');
 
 
 router.get('/', function(req, res) {
@@ -11,5 +12,32 @@ router.get('/', function(req, res) {
 //     res.json(users);
 //   })
 // })
+
+//first we get route with url name and we added a param ':id'
+router.get('/products/:id', function(req, res, next) {
+  Product
+    .find({ //the QUERY 'objectId' from the models in the product.js 
+      category: req.params.id //this is why we use req.params ->access by the id of the category
+    })
+    .populate('category') //.populate ONLY if a data type is an `objectId` (also from models / product.js)
+    .exec(function(err, products) {//if there are more than 1 method we use exec
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products //what we are looping [] from in categories.ejs
+      });
+    });
+});
+
+//SHOW ME SINGLE product page
+router.get('/product/:id', function(req, res, next) {//QUERY based on id(particular product)
+  Product.findById({
+    _id: req.params.id
+  }, function(err, product) { //product is cb and will pass it to the ejs file 'product.ejs'
+    if (err) return next(err);
+    res.render('main/product', {
+      product: product
+    });
+  });
+});
 
 module.exports = router;
